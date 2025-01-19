5d28f14dd53adb7fdb7148d82ade93b95fe15e0e
Sam O'Brien <sam@getmosh.com>
2025-01-19 20:20:07 +1000

    Base metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,30 +15,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    metadataBase: new URL("https://example.com"),
-    title: {
-        default: "Next.js Portfolio Starter",
-        template: "%s | Next.js Portfolio Starter",
-    },
-    description: "This is my portfolio.",
-    openGraph: {
-        title: "My Portfolio",
-        description: "This is my portfolio.",
-        url: "https://example.com",
-        siteName: "My Portfolio",
-        locale: "en_AU",
-        type: "website",
-    },
+    metadataBase: new URL("https://todayilearned.au"),
+    title: "Today I Learned",
+    description: "Collection of tools and articles about things I've learned",
     robots: {
         index: true,
         follow: true,
-        googleBot: {
-            index: true,
-            follow: true,
-            "max-video-preview": -1,
-            "max-image-preview": "large",
-            "max-snippet": -1,
-        },
     },
 };
 
