5e603afa66e0d42a2dd724d239cc1b57ae33991c
Sam O'Brien <sam@getmosh.com>
2025-01-23 10:15:07 +1000

    Semibold tab font and extra gap

--- src/components/ui/tab.tsx
+++ src/components/ui/tab.tsx
@@ -6,8 +6,8 @@ type TabProps = {
 function Tab({ title, children }: TabProps) {
     return (
         <section className="flex flex-1 flex-col gap-4">
-            <h1 className="text-2xl text-primary">{title}</h1>
-            <div className="flex flex-col gap-1">{children}</div>
+            <h1 className="text-2xl font-semibold text-primary">{title}</h1>
+            <div className="flex flex-col gap-2">{children}</div>
         </section>
     );
 }
