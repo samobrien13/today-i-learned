924f2be8a3531dd1ccdfd422705dc287d15c3b79
Sam O'Brien <sam@getmosh.com>
2025-01-27 13:42:21 +1000

    Add security headers

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,57 @@
 import type { NextConfig } from "next";
 
+const CONTENT_SECURITY_POLICY = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self' data:;
+`;
+
+const securityHeaders = [
+    {
+        key: "Content-Security-Policy",
+        value: CONTENT_SECURITY_POLICY.replace(/\n/g, ""),
+    },
+    {
+        key: "Referrer-Policy",
+        value: "origin-when-cross-origin",
+    },
+    {
+        key: "X-Frame-Options",
+        value: "DENY",
+    },
+    {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+    },
+    {
+        key: "X-DNS-Prefetch-Control",
+        value: "on",
+    },
+    {
+        key: "Strict-Transport-Security",
+        value: "max-age=31536000; includeSubDomains; preload",
+    },
+    {
+        key: "Permissions-Policy",
+        value: "",
+    },
+];
+
 const nextConfig: NextConfig = {
     experimental: {
         reactCompiler: true,
+    },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: securityHeaders,
+            },
+        ];
     },
 };
 
