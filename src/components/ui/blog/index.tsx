2affd61bd11b56f33c95783e4e12592b1363e3c5
Sam O'Brien <sam@getmosh.com>
2025-02-09 18:59:48 +1000

    Delete duplicate file

--- src/components/ui/blog/index.tsx
+++ src/components/ui/blog/index.tsx
@@ -1,13 +1,12 @@
 import { formatDate } from "@/lib/date";
+import { Link } from "../link";
+import { Badge } from "../badge";
+import { BlogData } from "@/data/blog";
+import Routes from "@/constants/Routes";
 
-type BlogProps = {
-    title: string;
-    date: string;
-    slug: string;
-    children: React.ReactNode;
-};
+type BlogProps = BlogData;
 
-function Blog({ title, date, slug, children }: BlogProps) {
+function Blog({ title, date, slug, component, tags }: BlogProps) {
     return (
         <article className="flex flex-1 flex-col gap-4">
             <header>
@@ -28,17 +27,24 @@ function Blog({ title, date, slug, children }: BlogProps) {
                     {formatDate(date)}
                 </p>
             </header>
-            <div>{children}</div>
+            <div>{component}</div>
+            {tags.length > 0 ? (
+                <div className="flex flex-row flex-wrap gap-2 pb-4">
+                    {Array.from(new Set(tags)).map((tag) => {
+                        return (
+                            <Link key={tag} href={Routes.RANTS([tag])}>
+                                <Badge variant="secondary">{tag}</Badge>
+                            </Link>
+                        );
+                    })}
+                </div>
+            ) : null}
         </article>
     );
 }
 
-export function BlogSection({ children }: { children: React.ReactNode }) {
-    return <section className="flex flex-col">{children}</section>;
-}
-
 export function BlogHeading({ children }: { children: React.ReactNode }) {
-    return <h2 className="pb-2 text-xl">{children}</h2>;
+    return <h2 className="pb-2 pt-2 text-xl">{children}</h2>;
 }
 
 export function BlogSubHeading({ children }: { children: React.ReactNode }) {
