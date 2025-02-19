3784b81111f56103451d791c05b9b6db13478687
Sam O'Brien <sam@getmosh.com>
2025-02-20 06:10:36 +1000

    Add linkedin

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -4,14 +4,24 @@ import { Link } from "@/components/ui/link";
 function Footer() {
     return (
         <footer className="mt-12 flex flex-col gap-4">
-            <Link
-                className="flex items-center"
-                external
-                href="https://github.com/samobrien13"
-            >
-                <ArrowUpRight strokeWidth={1} />
-                <span className="ml-2 h-7">github</span>
-            </Link>
+            <div className="flex flex-row gap-2">
+                <Link
+                    className="flex items-center"
+                    external
+                    href="https://github.com/samobrien13"
+                >
+                    <ArrowUpRight strokeWidth={1} />
+                    <span className="ml-2 h-7">github</span>
+                </Link>
+                <Link
+                    className="flex items-center"
+                    external
+                    href="https://www.linkedin.com/in/sam-o-brien-8a48a0a1/"
+                >
+                    <ArrowUpRight strokeWidth={1} />
+                    <span className="ml-2 h-7">linkedin</span>
+                </Link>
+            </div>
             <p>© {new Date().getFullYear()} MIT Licensed</p>
         </footer>
     );
