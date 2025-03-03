154f1f56ed3d2b73e0bee42ef0131a157b2db36d
Sam O'Brien <sam@getmosh.com>
2025-03-03 12:51:45 +1000

    Update schema and handling

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,12 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "@/env";
+import * as schema from "./schema";
 
 const connectionString = env.DATABASE_URL;
 
 export const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client);
+export const db = drizzle({
+    client,
+    schema,
+});
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,29 +5,41 @@ import {
     serial,
     text,
     timestamp,
-    varchar,
+    uniqueIndex,
 } from "drizzle-orm/pg-core";
 
 export const blogComments = pgTable(
     "blog_comments",
     {
         id: serial("id").primaryKey(),
+        anonymous_id: text("anonymous_id"),
         slug: text("slug"),
-        author: varchar("author", { length: 256 }),
         comment: text("comment"),
-        approved: boolean("approved").default(false),
         created_at: timestamp("created_at").defaultNow(),
     },
-    (table) => [index("blog_comments_slug_index").on(table.slug)],
+    (table) => [
+        index("blog_comments_slug_index").on(table.slug),
+        uniqueIndex("blog_comments_anonymous_id_index").on(
+            table.anonymous_id,
+            table.slug,
+        ),
+    ],
 );
 
 export const blogLikes = pgTable(
     "blog_likes",
     {
         id: serial("id").primaryKey(),
+        anonymous_id: text("anonymous_id"),
         slug: text("slug"),
         value: boolean("value"), // true for like, false for dislike
         created_at: timestamp("created_at").defaultNow(),
     },
-    (table) => [index("blog_likes_slug_index").on(table.slug)],
+    (table) => [
+        index("blog_likes_slug_index").on(table.slug),
+        uniqueIndex("blog_likes_anonymous_id_index").on(
+            table.anonymous_id,
+            table.slug,
+        ),
+    ],
 );
