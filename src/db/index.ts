25ef40ec2ce8bc0fd6dd0c14ef4044dbcc711cd8
Sam O'Brien <sam@getmosh.com>
2025-03-03 17:49:19 +1000

    Change connection info

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,9 +3,15 @@ import postgres from "postgres";
 import { env } from "@/env";
 import * as schema from "./schema";
 
-const connectionString = env.DATABASE_URL;
+const connectionString = env.DATABASE_URL + "?sslmode=require";
 
-export const client = postgres(connectionString, { prepare: false });
+export const client = postgres(connectionString, {
+    max: 1,
+    idle_timeout: 5,
+    connect_timeout: 5,
+    prepare: false,
+});
+
 export const db = drizzle({
     client,
     schema,
