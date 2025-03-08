5796f915630b8f5685910ef10013ce6a9dd7b799
Sam O'Brien <sam@getmosh.com>
2025-03-08 14:22:37 +1000

    Add more to about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+import { BlogParagraph } from "@/components/ui/blog";
 import { Link } from "@/components/ui/link";
 import Tab from "@/components/ui/tab";
 import { Metadata } from "next";
@@ -12,33 +13,38 @@ export const metadata: Metadata = {
 export default function About() {
     return (
         <Tab title="About">
-            <p>
+            <BlogParagraph>
                 Hi, I&apos;m Sam, and this is a collection of things I&apos;ve
                 learned. I write about programming, web development, and other
                 topics that interest me.
-            </p>
-            <p>
+            </BlogParagraph>
+            <BlogParagraph>
                 I have been working professionally as a software engineer for
                 over 10 years, and worked in a variety of roles in Australia and
                 around the world.
-            </p>
-            <p>
+            </BlogParagraph>
+            <BlogParagraph>
                 I spend most of my time working with{" "}
                 <Link href="https://nextjs.org/">Next.js</Link> and{" "}
                 <Link href="https://expo.dev/">Expo</Link>, but I often venture
                 into other areas.
-            </p>
-            <p>
+            </BlogParagraph>
+            <BlogParagraph>
                 I also create&nbsp;
                 <Link href="/tools">tools</Link> that I find useful.
-            </p>
-            <p>
+            </BlogParagraph>
+            <BlogParagraph>
+                This site is aimed to be a good resource for copying the latest
+                good Next.js patterns and practices. I will use it as a test bed
+                for new library updates and ideas.
+            </BlogParagraph>
+            <BlogParagraph>
                 I always aspire to be{" "}
                 <Link href="https://grugbrain.dev" external>
                     grug
                 </Link>
                 .
-            </p>
+            </BlogParagraph>
         </Tab>
     );
 }
