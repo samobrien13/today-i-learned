01239a6c32fe3965a64f8ba710a9dca371ad6c19
Sam O'Brien <sam@getmosh.com>
2025-03-09 07:45:21 +1000

    test

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -4,29 +4,10 @@ export function formatDate(date: Date | string): string {
     }
 
     const diff = (new Date().getTime() - date.getTime()) / 1000;
-    const minutes = Math.floor(diff / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    const months = Math.floor(days / 30);
-    const years = Math.floor(months / 12);
     const rtf = new Intl.RelativeTimeFormat("en-GB", {
         numeric: "auto",
         style: "long",
     });
 
-    if (years > 0) {
-        return Intl.DateTimeFormat("en-GB", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-        }).format(date);
-    }
-    if (months > 0) {
-        return Intl.DateTimeFormat("en-GB", {
-            month: "short",
-            day: "numeric",
-        }).format(date);
-    }
-
-    return rtf.format(0 - days, "day");
+    return rtf.format(0 - diff, "second");
 }
