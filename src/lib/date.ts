85fcc79c93b66aad881218a11f6bc80f82b9a677
Sam O'Brien <sam@getmosh.com>
2025-03-09 07:51:23 +1000

    Assume date UTC

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,6 +1,6 @@
 export function formatDate(date: Date | string): string {
     if (typeof date === "string") {
-        date = new Date(date);
+        date = new Date(date + 'T00:00:00.000Z');
     }
 
     const diff = (new Date().getTime() - date.getTime()) / 1000;
