cea9d1a0c1f9e9c4b6edd31899bd3e68950f67b2
Sam O'Brien <sam@getmosh.com>
2025-03-15 16:22:14 +1000

    Add RSS feed

---
+++ app/rants/feed/route.ts
@@ -0,0 +1,35 @@
+import { BLOG_ARTICLES } from "@/data/blog";
+
+import RSS from "rss";
+
+const siteUrl = "https://todayilearned.au";
+
+const feedOptions: RSS.FeedOptions = {
+    title: "Rants | Today I Learned",
+    description: "Read my rants about all sorts of things.",
+    site_url: siteUrl,
+    feed_url: `${siteUrl}/rants/feed`,
+    pubDate: new Date(),
+    copyright: `All rights unreserved ${new Date().getFullYear()}`,
+};
+
+export async function GET() {
+    const feed = new RSS(feedOptions);
+
+    BLOG_ARTICLES.map((post) => {
+        feed.item({
+            title: post.title,
+            description: post.description,
+            url: `${siteUrl}/blog/${post.slug}`,
+            date: post.date,
+        });
+    });
+
+    return new Response(feed.xml({ indent: true }), {
+        status: 200,
+        headers: {
+            "Content-Type": "text/xml",
+            "Cache-Control": "s-maxage=86400, stale-while-revalidate",
+        },
+    });
+}
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,24 +1,20 @@
 import { Link } from "@/components/ui/link";
 import { Github, LinkedIn } from "@/components/ui/icons";
 import Routes from "@/constants/Routes";
+import { RssIcon } from "lucide-react";
 
 function Footer() {
     return (
         <footer className="mt-12 flex flex-col gap-4">
-            <div className="flex flex-row gap-2">
-                <Link
-                    className="flex items-center"
-                    external
-                    href="https://github.com/samobrien13"
-                >
+            <div className="flex flex-row items-center gap-2">
+                <Link external href="https://github.com/samobrien13">
                     <Github />
                 </Link>
-                <Link
-                    className="flex items-center"
-                    external
-                    href="https://www.linkedin.com/in/samobrien13"
-                >
+                <Link external href="https://www.linkedin.com/in/samobrien13">
                     <LinkedIn />
+                </Link>
+                <Link href="https://todayilearned.au/rants/feed">
+                    <RssIcon />
                 </Link>
                 <Link href={Routes.ABOUT} className="px-3 py-2">
                     about
