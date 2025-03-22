8054b6f723d115a6ebbd93c1fc02467240397688
Sam O'Brien <sam@getmosh.com>
2025-03-22 11:30:39 +1000

    Fix lint

--- src/hooks/use-element-dimensions.ts
+++ src/hooks/use-element-dimensions.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useLayoutEffect, useState } from "react";
 
 export function useElementDimensions(
     ref: React.RefObject<HTMLDivElement | null>,
@@ -8,16 +8,15 @@ export function useElementDimensions(
 ] {
     const [size, setSize] = useState({ width: 0, height: 0, top: 0, left: 0 });
 
-    function measure() {
+    const measure = useCallback(() => {
         if (!ref.current) return;
 
         setSize(ref.current.getBoundingClientRect());
-    }
+    }, [ref]);
 
-    // Note: This won't accurately reflect viewport size changes
-    useEffect(() => {
+    useLayoutEffect(() => {
         measure();
-    }, []);
+    }, [measure]);
 
     return [size, measure];
 }
