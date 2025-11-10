290f10fcbd9b30ac3060fd4679bf2f3b2afa3ae9
Sam O'Brien <sam@getmosh.com>
2025-11-10 13:46:00 +1000

    Fix command

--- src/data/blog/rants/how-to-find-and-replace-from-the-command-line.tsx
+++ src/data/blog/rants/how-to-find-and-replace-from-the-command-line.tsx
@@ -46,7 +46,7 @@ function HowToFindAndReplaceFromTheCommandLine() {
                 The following command will do the job on OSX:
             </BlogParagraph>
             <CodeBlock language="bash">
-                {`LC_ALL=C find ./ -type f -exec sed -i '' -e & '/find text/replace text/' {} \\;`}
+                {`LC_ALL=C find ./ -type f -exec sed -i '' -e 's/find text/replace text/g' {} \\;`}
             </CodeBlock>
             <BlogUnorderedList>
                 <BlogListItem>
