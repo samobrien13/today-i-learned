b743d76432afa0eb4acd3a9992689ec94d8198ee
Sam O'Brien <sam@getmosh.com>
2026-02-21 08:56:47 +1000

    Move folder

--- src/data/blog/index.tsx
+++ src/data/blog/index.tsx
@@ -2,21 +2,21 @@ import { Metadata } from "next";
 import { ReactNode } from "react";
 import { ImageProps } from "next/image";
 
-import { PLEASE_DONT_VALIDATE_MY_NAME } from "./rants/please-dont-validate-my-name";
-import { HOW_TO_FIND_AND_REPLACE_FROM_THE_COMMAND_LINE } from "./rants/how-to-find-and-replace-from-the-command-line";
-import { DESIGN_SYSTEM } from "./rants/design-system";
-import { DOTFILES } from "./rants/dotfiles";
-import { HOW_TO_TELL_THE_TIME_THAI_STYLE } from "./rants/how-to-tell-the-time-thai-style";
-import { WHY_AI_WONT_TAKE_YOUR_JOB } from "./rants/why-ai-wont-take-your-job";
-import { HOW_TO_TEST_NEXT_JS_APP_ROUTER } from "./rants/how-to-test-next-js-app-router";
-import { HOW_MUCH_TIME_THE_RBA_RATE_CUT_WILL_SAVE_YOU } from "./rants/how-much-time-the-rba-rate-cut-will-save-you";
-import { HOW_TO_IMPLEMENT_DEEP_LINKS_WITH_EXPO_ROUTER } from "./rants/how-to-implement-deep-links-with-expo-router";
-import { PLEASE_USE_REACT_QUERY } from "./rants/please-use-react-query";
-import { THE_BEST_PULL_REQUEST_TEMPLATE } from "./rants/the-best-pull-request-template";
-import { REACT_QUERY_WITH_SERVER_ACTIONS } from "./rants/react-query-with-server-actions";
-import { KEYBOARD_SETUP } from "./rants/keyboard-setup";
-import { CONNECT_EXPO_DEV_BUILDS_REMOTELY } from "./rants/connect-expo-dev-builds-remotely";
-import { RULE_OF_3 } from "./rants/rule-of-3";
+import { PLEASE_DONT_VALIDATE_MY_NAME } from "./please-dont-validate-my-name";
+import { HOW_TO_FIND_AND_REPLACE_FROM_THE_COMMAND_LINE } from "./how-to-find-and-replace-from-the-command-line";
+import { DESIGN_SYSTEM } from "./design-system";
+import { DOTFILES } from "./dotfiles";
+import { HOW_TO_TELL_THE_TIME_THAI_STYLE } from "./how-to-tell-the-time-thai-style";
+import { WHY_AI_WONT_TAKE_YOUR_JOB } from "./why-ai-wont-take-your-job";
+import { HOW_TO_TEST_NEXT_JS_APP_ROUTER } from "./how-to-test-next-js-app-router";
+import { HOW_MUCH_TIME_THE_RBA_RATE_CUT_WILL_SAVE_YOU } from "./how-much-time-the-rba-rate-cut-will-save-you";
+import { HOW_TO_IMPLEMENT_DEEP_LINKS_WITH_EXPO_ROUTER } from "./how-to-implement-deep-links-with-expo-router";
+import { PLEASE_USE_REACT_QUERY } from "./please-use-react-query";
+import { THE_BEST_PULL_REQUEST_TEMPLATE } from "./the-best-pull-request-template";
+import { REACT_QUERY_WITH_SERVER_ACTIONS } from "./react-query-with-server-actions";
+import { KEYBOARD_SETUP } from "./keyboard-setup";
+import { CONNECT_EXPO_DEV_BUILDS_REMOTELY } from "./connect-expo-dev-builds-remotely";
+import { RULE_OF_3 } from "./rule-of-3";
 
 export type BlogData = {
     title: string;
