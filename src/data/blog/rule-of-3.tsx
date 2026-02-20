3c5592889c6c0c52064c067144c91a4e6be1d135
Sam O'Brien <sam@getmosh.com>
2026-02-21 09:16:52 +1000

    Image update

--- src/data/blog/rule-of-3.tsx
+++ src/data/blog/rule-of-3.tsx
@@ -3,13 +3,13 @@ import { BlogData } from "@/data/blog";
 
 export const RULE_OF_3: BlogData = {
     title: "Rule of 3",
-    description: "The rule of three (programming)",
+    description: "The rule of three and why DRY isn't always the best approach",
     date: "2026-02-21",
     slug: "rule-of-3",
     tags: ["programming", "refactoring"],
     image: {
-        src: "/images/rants/lily36.jpg",
-        alt: "Image of three lily pads",
+        src: "/images/rants/triangle.png",
+        alt: "Rule of 3",
     },
     component: <RuleOf3 />,
 };
@@ -18,10 +18,14 @@ function RuleOf3() {
     return (
         <>
             <BlogParagraph>
-                The &quot;rule of three&quot; in programming is a heuristic which states that a piece of code can be copied once, but if it&apos;s copied a third time, it should be refactored into a new abstraction.
+                The &quot;rule of three&quot; in programming is a heuristic
+                which states that a piece of code can be copied once, but if
+                it&apos;s copied a third time, it should be refactored into a
+                new abstraction.
             </BlogParagraph>
             <BlogParagraph>
-                This principle helps in reducing code duplication and improving maintainability.
+                This principle helps in reducing code duplication and improving
+                maintainability.
             </BlogParagraph>
         </>
     );
