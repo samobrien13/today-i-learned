c01c99f82f8fa513b52cabe57934ca6330b3d003
Sam O'Brien <sam@getmosh.com>
2026-03-06 19:18:29 +1000

    Final

--- src/data/blog/normalised-deviance.tsx
+++ src/data/blog/normalised-deviance.tsx
@@ -6,7 +6,7 @@ import {
 import { BlogData } from "@/data/blog";
 
 export const NORMALISED_DEVIANCE: BlogData = {
-    title: "Normalised Deviance in Software Development",
+    title: "Normalised Deviance",
     description:
         "What is normalised deviance, how does it apply to software development, and how can we avoid it?",
     date: "2026-03-01",
@@ -36,20 +36,21 @@ function NormalisedDeviance() {
             <BlogHeading>What is Normalised Deviance?</BlogHeading>
             <BlogParagraph>
                 Normalised deviance occurs when people become so accustomed to
-                deviant behaviour that they start to see it as normal.{" "}
+                deviant behaviour that they start to see it as normal. Deviance
+                builds up and becomes so ingrained in the culture that people no
+                longer question it.
             </BlogParagraph>
             <BlogParagraph>
                 Some examples I have seen over the years include:
                 <ul>
                     <li>
                         Production, staging and development environments all
-                        connecting to a production CRM instance.
+                        connecting to the same production CRM instance.
                     </li>
                     <li>
-                        Hardcoding API keys and secrets in code, with if else
-                        statements for the environment
+                        Developers manually re-running failed jobs in a queue
+                        each day
                     </li>
-                    <li>Manually re-running failed jobs in a queue</li>
                     <li>
                         Commenting out code in order to make your local
                         environment work
@@ -68,7 +69,55 @@ function NormalisedDeviance() {
             <BlogParagraph>
                 If a new starter even gives a hint of discomfort at the mention
                 of something, take note and investigate further. It may be an
-                indictaion of a bigger problem.
+                indictaion of a problem going unnoticed.
+            </BlogParagraph>
+            <BlogSubHeading>Broken Windows Theory</BlogSubHeading>
+            <BlogParagraph>
+                Ever heard someone say, &quot;Oh, don&apos;t worry, that only
+                happens in staging&quot;? This is a classic example of the
+                broken windows theory, coined by criminologists George Kelling
+                and James Wilson in 1982. The idea is that if you allow people
+                to get away with petty crimes such as graffiti, litter and
+                broken windows, it creates an environment where people are more
+                likely to commit more serious crimes. In the same way, if you
+                allow small inconvenience to linger and allow people to get away
+                with small things going wrong, it creates a culture where people
+                are more likely to allow bigger things to go wrong.
+            </BlogParagraph>
+            <BlogParagraph>
+                It&apos;s important to address small issues as soon as they
+                arise, and to create a culture where people feel comfortable
+                raising concerns and reporting issues. This way you can prevent
+                small issues from becoming big problems. Fixing small issues
+                almost always pays for itself in the long run. That staging data
+                you spend 10 minutes fixing saves the next person 2 hours of
+                debugging and investigation. The README update you make to
+                clarify a process saves the next new hire a day of confusion and
+                asking questions.
+            </BlogParagraph>
+            <BlogSubHeading>Always be fixing</BlogSubHeading>
+            <BlogParagraph>
+                Whenever you go to work on something, always be looking for ways
+                to improve it. Ask yourself, &quot;If we were designing this
+                system today, would we accept this flaw?&quot; If the answer is
+                no, then take some time to make it better. This doesn&apos;t
+                mean you need to spend hours on it, but even a small improvement
+                can make a big difference in the long run. Write a unit test.
+                Add a comment. Turn a one liner into a sensibly named variable.
+                Break up a large function into smaller ones. Each small
+                improvement adds up over time, and the more people that adopt
+                this mindset creates a snowball effect.
+            </BlogParagraph>
+            <BlogHeading>Conclusion</BlogHeading>
+            <BlogParagraph>
+                Normalised deviance is a dangerous phenomenon that can lead to a
+                culture of complacency and a lack of accountability. A negative
+                feedback loop can be created where people become so accustomed
+                to deviant that even the most obvious and serious issues can be
+                overlooked. By recognising the signs of normalised deviance and
+                taking steps to avoid it, we can create a culture of
+                accountability and continuous improvement, which can lead to
+                better outcomes for everyone involved.
             </BlogParagraph>
         </>
     );
