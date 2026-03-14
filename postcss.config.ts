abb905614bf612c76fd459d72d8f9f5aedc0c25d
Sam O'Brien <sam@getmosh.com>
2026-03-14 11:09:45 +1000

    Restore css for now

--- postcss.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "postcss-load-config";
-import tailwindcss from '@tailwindcss/postcss';
-
-
-const config: Config = {
-    plugins: [tailwindcss()],
-};
-
-export default config;
